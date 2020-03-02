74fdb962fc6a79ff540bbcb192ae5ee5f163f4e4
deepakbhavsar43 <deepakbhavsar43@gmail.com>
2020-03-02 22:30:59 +0530

    Add : Addedfunction for training dataset.

--- malaria_Classification.py
+++ malaria_Classification.py
@@ -4,6 +4,11 @@ from variables import *
 
 # Loading the dataset
 dataset = pd.read_csv(data_dir+"\\"+Parasitized)
-print(dataset.head())
+# print(dataset)
 
 # Split into training and test dataset
+x = dataset.drop(["Label"],axis=1)
+# print(x)
+y=dataset["Label"]
+
+x_train, x_test, y_train, y_test = train_test_split
