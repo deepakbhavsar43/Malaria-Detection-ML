4326dba47c8f91820f8044ca9c3f1518ff087988
deepakbhavsar43 <deepakbhavsar43@gmail.com>
2022-07-14 13:53:25 +0530

    Update: malaria_classification.py

--- malaria_Classification.py
+++ malaria_Classification.py
@@ -1,14 +1,16 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 from variables import *
+
 # import GUI_Import_CSV
 
 # Loading the dataset
-dataset = pd.read_csv(data_dir+"\\"+Parasitized)
-# print(dataset)
+dataset = pd.read_csv(data_dir + "\\" + Parasitized)
+print(dataset)
 
 # Split into training and test dataset
-x = dataset.drop(["Label"],axis=1)
-# print(x)
-y=dataset["Label"]
-
-x_train, x_test, y_train, y_test = train_test_split
+x = dataset.drop(["Label"], axis=1)
+print(x)
+y = dataset["Label"]
+# print(y)
+# x_train, x_test, y_train, y_test = train_test_split
